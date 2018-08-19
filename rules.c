c51cc182e789962bffca036883ddf3e0398f9180
Steph Ridnell <steph.ridnell@gmail.com>
2018-08-19 18:07:17 +1000

    Added move validate

--- rules.c
+++ rules.c
@@ -28,6 +28,9 @@
  * different rules when it comes to the first move of the game!
  **/
 BOOLEAN is_valid_move(struct player* currentPlayer, char word[], const char location[], struct move* theMove, BOOLEAN isFirst) {
+  char *coordCopy, *currentToken, *xCoord, *yCoord, *direction;
+  int tokenCount = 1, y = 0, x = 0;
+  
   /* Firstly, you'll need to check that the word exists in the dictionary */
   if (!lookup_word(currentPlayer->theGame->allWords, word)) {
     error_print("Word not found in dictionary.\n");
@@ -35,7 +38,108 @@ BOOLEAN is_valid_move(struct player* currentPlayer, char word[], const char loca
   }
   
   /* Next tokenize the move string and validate it */
-  /* Finally, test that the word specified can be made on the board given the starting location and the player's hand */
+  if (!(coordCopy = strdup(location))) {
+    error_print("Error copying coord string\n");
+    /* return new tile error (-1) */
+    return FALSE;
+  }
+
+  /* get the tokens */
+  currentToken = strtok(coordCopy, ":");
+  while (currentToken) {
+    if (tokenCount == 1) {
+      /* store the x coord */
+      xCoord = strdup(currentToken);
+      if (!xCoord) {
+        error_print("Error getting x coord\n");
+        return FALSE;
+      }
+    } else if (tokenCount == 2) {
+      /* store the y coord */
+      yCoord = strdup(currentToken);
+      if (!yCoord) {
+        error_print("Error getting y coord\n");
+        return FALSE;
+      }
+    } else if (tokenCount == 3) {
+      /* store the direction */
+      direction = strdup(currentToken);
+      if (!direction) {
+        error_print("Error getting direction\n");
+        return FALSE;
+      }
+    } else {
+      /* if it gets here it means there are too many tokens so we error */
+      error_print("Error. Invalid location format encountered. Too many tokens: %s\n", location);
+      return FALSE;
+    }
+
+    /* increment token count */
+    tokenCount++;
+
+    /* proceed to next token */
+    currentToken = strtok(NULL, ":");
+  }
+
+  if (tokenCount != 4) {
+    error_print("Error. Invalid location format encountered. Too few tokens: %s\n", location);
+    return FALSE;
+  }
+
+  /* free mem allocated to the string copy we made */
+  free(coordCopy);
+
+  /* make COORDS int */
+  if (!str_to_int(xCoord, &x)) {
+    error_print("Error. Invalid tile format encountered. X coord needs to be a number: %s\n", location);
+    return FALSE;
+  }
+
+  if (!str_to_int(yCoord, &y)) {
+    error_print("Error. Invalid tile format encountered. Y coord needs to be a number: %s\n", location);
+    return NEW_TILE_ERROR;
+  }
+
+  /* decerement coords cos arrays start at 0 */
+  x--;
+  y--;
+
+  /* make sure within board boundries */
+  if (x > currentPlayer->theGame->theBoard.boardSize || x < 0) {
+    error_print("Error. X coord not witin board bounds.\n");
+    return FALSE;
+  }
+
+  if (y > currentPlayer->theGame->theBoard.boardSize || y < 0) {
+    error_print("Error. Y coord not witin board bounds.\n");
+    return FALSE;
+  }
+
+  /* save coord values in the move */
+  theMove->x = x;
+  theMove->y = y;
+
+  /* validate direction and make sure word fits */
+  if (strcmp(direction, "h") == 0 || strcmp(direction, "H") == 0) {
+    theMove->dir = DIR_HORIZONTAL;
+    if (theMove->x + strlen(word) > currentPlayer->theGame->theBoard.boardSize) {
+      error_print("Error. Word will not fit on board.\n");
+      return FALSE;
+    }
+  } else if (strcmp(direction, "v") == 0 || strcmp(direction, "V") == 0) {
+    theMove->dir = DIR_VERTICAL;
+    if (theMove->y + strlen(word) > currentPlayer->theGame->theBoard.boardSize) {
+      error_print("Error. Word will not fit on board.\n");
+      return FALSE;
+    }
+  } else {
+    error_print("Error. Invalid direction.\n");
+    return FALSE;
+  }
+
+  /* TODO Finally, test that the word specified can be made 
+  on the board given the starting location and the player's hand */
+
   return TRUE;
 }
 
